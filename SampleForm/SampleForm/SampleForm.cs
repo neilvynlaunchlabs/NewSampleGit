14645aa426a435522b965f7912fb36da785e84a6
Neilvyn Tesio <neilvyn.tesio@launchlabs.ph>
2015-12-09 13:44:36 +0800

    Merged login-feature to next.
    Signed-off-by: Neilvyn Tesio <neilvyn.tesio@launchlabs.ph>

--- SampleForm/SampleForm/SampleForm.cs
+++ SampleForm/SampleForm/SampleForm.cs
@@ -15,7 +15,7 @@ namespace SampleForm
 					Children = {
 						new Label {
 							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
+							Text = "Gwapo si Neil"
 						}
 					}
 				}
